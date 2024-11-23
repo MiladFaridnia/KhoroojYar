b1fd7150d7f3dbbd16baf5b49359a919375b926c
Amirhosein Baratali <a.baratali@jiring.ir>
2024-11-23 10:48:04 +0330

    Calculate vacation time needed when enter time is after 9

--- app/src/main/java/com/faridnia/khoroojyar/ui/ExitTimeViewModel.kt
+++ app/src/main/java/com/faridnia/khoroojyar/ui/ExitTimeViewModel.kt
@@ -8,27 +8,37 @@ import java.time.format.DateTimeParseException
 
 class ExitTimeViewModel : ViewModel() {
 
-    // Input and output states
     var enterTimeInput = mutableStateOf("")
         private set
     var exitTime = mutableStateOf("")
         private set
+    var vacationMessage = mutableStateOf("")
+        private set
 
     private val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
 
-    // Update the input state
     fun onEnterTimeChange(newTime: String) {
         enterTimeInput.value = newTime
     }
 
-    // Calculate the exit time
     fun calculateExitTime() {
         try {
             val enterTime = LocalTime.parse(enterTimeInput.value, timeFormatter)
-            val exitTimeCalculated = enterTime.plusHours(8).plusMinutes(45)
-            exitTime.value = exitTimeCalculated.format(timeFormatter)
+            if (enterTime.isAfter(LocalTime.of(9, 0))) {
+                val vacationStart = LocalTime.of(9, 0)
+                vacationMessage.value =
+                    "Vacation time needed: ${vacationStart.format(timeFormatter)}_${
+                        enterTime.format(timeFormatter)
+                    }"
+                exitTime.value = "17:45"
+            } else {
+                vacationMessage.value = ""
+                val exitTimeCalculated = enterTime.plusHours(8).plusMinutes(45)
+                exitTime.value = exitTimeCalculated.format(timeFormatter)
+            }
         } catch (e: DateTimeParseException) {
             exitTime.value = "Invalid time format"
+            vacationMessage.value = ""
         }
     }
 }
