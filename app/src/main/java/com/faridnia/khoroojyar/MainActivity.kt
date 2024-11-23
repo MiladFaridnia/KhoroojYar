cc031d74f685c17e88e01822124f7cfb8f8d544e
Milad Faridnia <miladfaridnia@gmail.com>
2024-11-23 09:26:21 +0330

    Initial commit

---
+++ app/src/main/java/com/faridnia/khoroojyar/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.faridnia.khoroojyar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import com.faridnia.khoroojyar.ui.screen.ExitTimeCalculator
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KhoroojYar()
+        }
+    }
+}
+
+@Composable
+fun KhoroojYar() {
+    MaterialTheme {
+        ExitTimeCalculator()
+    }
+}
+
