d2d5bc3db8ce24228e29aba1e1064f93e4b8d22f
Milad Faridnia <miladfaridnia@gmail.com>
2024-11-30 14:55:05 +0330

    Feat: V-1.8.0 Add UiText class for dynamic strings and string resource

---
+++ app/src/main/java/com/faridnia/khoroojyar/util/UiText.kt
@@ -0,0 +1,16 @@
+package com.faridnia.khoroojyar.util
+
+import android.content.Context
+
+/** Thanks to our Master Phillip */
+sealed class UiText {
+    data class DynamicString(val text: String) : UiText()
+    data class StringResource(val resId: Int) : UiText()
+
+    fun asString(context: Context): String {
+        return when (this) {
+            is DynamicString -> text
+            is StringResource -> context.getString(resId)
+        }
+    }
+}
